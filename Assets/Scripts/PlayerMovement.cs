0d237749cf120942572326b8a9b10091ccad3d20
Kartik <kartik242421@gmail.com>
2024-03-18 00:48:00 +0530

    player input + camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,41 +6,51 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    [SerializeField] private float velocity = 1.5f;
-    private Rigidbody rb;
+    public CharacterController controller; // Reference to the Character Controller
+    private PlayerInput playerInput; // Reference to the PlayerInput component
 
-    public Vector3 movement;
-    // Start is called before the first frame update
+    Camera mainCam;
+    CameraController cameraController;
+
+    [System.Obsolete]
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        mainCam = Camera.main;
+        controller = GetComponent<CharacterController>(); // Assigning the Character Controller reference
+        playerInput = GetComponent<PlayerInput>(); // Assigning the PlayerInput component
+        cameraController = FindObjectOfType<CameraController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(movement * speed * Time.deltaTime);
-    }
-    public void OnMovement(InputValue val)
-    {
+        Vector2 moveInput = GetMovementInput();
+        Vector3 moveDirection = CalculateMoveDirection(moveInput);
 
-        Setmovement(val.Get<Vector2>());
-    }
+        // Move the character using Character Controller
+        controller.Move(moveDirection * speed * Time.deltaTime);
 
-    public void OnMovementEvent(InputAction.CallbackContext ctx)
-    {
-        Setmovement(ctx.ReadValue<Vector2>());
-    }
-
-    private void Setmovement(Vector2 moveVal)
-    {
-        movement = moveVal;
-        movement.z = movement.y;
-        movement.y = 0;
+        if (moveInput.magnitude != 0 && cameraController != null)  //here we give the value for the x input to change camera rotation
+        {
+            cameraController.AddYawInput(moveInput.x);
+        }
     }
 
-    public void OnJump()
+    private Vector2 GetMovementInput()
     {
-        rb.velocity = Vector2.up * velocity;
+        // Get movement input from PlayerInput component
+        return playerInput.actions["PlayerActionMap/Movement"].ReadValue<Vector2>();
+    }
+
+    private Vector3 CalculateMoveDirection(Vector2 moveInput)
+    {
+        // Calculate movement based on camera's orientation
+        Vector3 rightDir = mainCam.transform.right;
+        Vector3 upDir = Vector3.Cross(rightDir, Vector3.up);
+        Vector3 moveDirection = (rightDir * moveInput.x + upDir * moveInput.y).normalized;
+
+        // Keep the y-component constant
+        moveDirection.y = 0f;
+
+        return moveDirection;
     }
 }
